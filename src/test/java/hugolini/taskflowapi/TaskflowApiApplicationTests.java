d990f90a9afa8fe9b9acca201570bf8529afb44f
matheus.carneiro <matheus.carneiro@darede.com.br>
2024-12-07 20:02:59 -0300

    starting task flow api

---
+++ src/test/java/hugolini/taskflowapi/TaskflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hugolini.taskflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
