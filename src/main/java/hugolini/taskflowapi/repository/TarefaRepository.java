fcc02454d12cc7cbf2eb17dde33655446f2e626b
matheus.carneiro <matheus.carneiro@darede.com.br>
2024-12-09 23:57:30 -0300

    feat: repository

---
+++ src/main/java/hugolini/taskflowapi/repository/TarefaRepository.java
@@ -0,0 +1,10 @@
+package hugolini.taskflowapi.repository;
+
+import hugolini.taskflowapi.model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+}
