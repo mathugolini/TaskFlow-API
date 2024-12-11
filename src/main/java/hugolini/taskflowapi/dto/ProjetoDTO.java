72f6ced9154e9e9d027402ae0eacb526c466e1d5
matheus.carneiro <matheus.carneiro@darede.com.br>
2024-12-11 11:38:24 -0300

    feat: DTO projeto e tarefa

--- src/main/java/hugolini/taskflowapi/dto/ProjetoDTO.java
+++ src/main/java/hugolini/taskflowapi/dto/ProjetoDTO.java
@@ -1,2 +1,33 @@
-package hugolini.taskflowapi.dto;public class ProjetoDTO {
+package hugolini.taskflowapi.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class ProjetoDTO {
+
+    @NotBlank(message = "O nome do projeto é obrigatório.")
+    private String nome;
+    private String descricao;
+
+    public ProjetoDTO () {}
+
+    public ProjetoDTO(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
--- src/main/java/hugolini/taskflowapi/dto/TarefaDTO.java
+++ src/main/java/hugolini/taskflowapi/dto/TarefaDTO.java
@@ -1,2 +1,44 @@
-package hugolini.taskflowapi.dto;public class TarefaDTO {
+package hugolini.taskflowapi.dto;
+
+import hugolini.taskflowapi.model.Projeto;
+import jakarta.validation.constraints.NotBlank;
+
+public class TarefaDTO {
+
+    @NotBlank(message = "O título da tarefa é obrigatório.")
+    private String titulo;
+    private String descricao;
+    private String projetoId;
+
+    public TarefaDTO() { }
+
+    public TarefaDTO(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.projetoId = projetoId;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getProjetoId() {
+        return projetoId;
+    }
+
+    public void setProjetoId(String projetoId) {
+        this.projetoId = projetoId;
+    }
 }
