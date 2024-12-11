57da1fda3231f0a6bbfed479de7222bd44ed9a41
matheus.carneiro <matheus.carneiro@darede.com.br>
2024-12-11 11:43:00 -0300

    feat: pagination

--- src/main/java/hugolini/taskflowapi/repository/ProjetoRepository.java
+++ src/main/java/hugolini/taskflowapi/repository/ProjetoRepository.java
@@ -1,10 +1,22 @@
 package hugolini.taskflowapi.repository;
 
-
 import hugolini.taskflowapi.model.Projeto;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 @Repository
-public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
+public interface ProjetoRepository extends JpaRepository<Projeto, String> {
+
+    Page<Projeto> findAll(Pageable pageable);
+
+    Page<Projeto> findByNomeStartingWithIgnoreCase(String nome, Pageable pageable);
+
+    Page<Projeto> findByNomeIgnoreCase(String nome, Pageable pageable);
+
 }
--- src/main/java/hugolini/taskflowapi/repository/TarefaRepository.java
+++ src/main/java/hugolini/taskflowapi/repository/TarefaRepository.java
@@ -1,10 +1,21 @@
 package hugolini.taskflowapi.repository;
 
+import hugolini.taskflowapi.enums.StatusTarefaEnum;
+import hugolini.taskflowapi.model.Projeto;
 import hugolini.taskflowapi.model.Tarefa;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+import java.util.List;
+import java.util.Optional;
 
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, String> {
+    Page<Tarefa> findAll(Pageable pageable);
+
+    Optional<Tarefa> findByTitulo(String titulo);
+
+    List<Tarefa> findByProjeto(Projeto projeto);
 }
