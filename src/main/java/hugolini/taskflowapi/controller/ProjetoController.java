4a32fcc4350b09b369f7d8c603a3441ad0e744d7
matheus.carneiro <matheus.carneiro@darede.com.br>
2024-12-11 21:39:24 -0300

    feat: annotations for swagger

--- src/main/java/hugolini/taskflowapi/controller/ProjetoController.java
+++ src/main/java/hugolini/taskflowapi/controller/ProjetoController.java
@@ -1,13 +1,12 @@
 package hugolini.taskflowapi.controller;
 
+import hugolini.taskflowapi.annotations.projeto.*;
 import hugolini.taskflowapi.dto.ProjetoDTO;
 import hugolini.taskflowapi.dto.TarefasPorStatusDTO;
-import hugolini.taskflowapi.enums.StatusTarefaEnum;
-import hugolini.taskflowapi.exceptions.ProjetoNaoEncontradoException;
 import hugolini.taskflowapi.model.Projeto;
-import hugolini.taskflowapi.model.Tarefa;
-import hugolini.taskflowapi.repository.TarefaRepository;
 import hugolini.taskflowapi.service.ProjetoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -15,54 +14,69 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/projetos")
+@Tag(name = "Projetos", description = "Operações para criar e gerenciar os projetos")
 public class ProjetoController {
 
     @Autowired
     private ProjetoService projetoService;
 
-    @Autowired
-    private TarefaRepository tarefaRepository;
-
+    @ApiResponseCriarProjeto
+    @Operation(summary = "Cria projeto",
+            description = "Este endpoint permite a criação de projetos com ID(gerado automaticamente), Nome(obrigatório), Descrição(opcional) e Data de criação(gerado automaticamente)" )
     @PostMapping
     public ResponseEntity<Projeto> criarProjeto(@Valid @RequestBody ProjetoDTO projeto) throws Exception {
         return ResponseEntity.status(HttpStatus.CREATED).body(projetoService.criarProjeto(projeto));
     }
 
+    @ApiResponseListarProjetos
+    @Operation(summary = "Lista todos os projetos com paginação e suas respectivas tarefas associadas",
+            description = "Este endpoint permite listar os projetos paginados (página e número de itens por página).")
     @GetMapping
     public ResponseEntity<Page<Projeto>> listarProjetos(@RequestParam Integer pagina,
                                                         @RequestParam Integer itens) throws Exception {
         return ResponseEntity.ok(projetoService.listarProjetos(pagina, itens));
     }
 
+    @ApiResponseBuscarProjetoPorId
+    @Operation(summary = "Lista projeto por ID e suas respectivas tarefas associadas",
+            description = "Este endpoint permite listar os projetos por ID.")
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Projeto>> buscarProjetoPorId(@PathVariable String id) throws Exception{
         return ResponseEntity.ok(projetoService.buscarProjetoPorId(id));
     }
 
+    @ApiResponseDeletarProjeto
+    @Operation(summary = "Deleta projeto por ID e suas respectivas tarefas associadas",
+            description = "Este endpoint permite deletar os projetos por ID.")
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deletarProjeto(@PathVariable String id) throws Exception{
         projetoService.deletarProjeto(id);
         return ResponseEntity.status(HttpStatus.OK).body("Projeto com ID " + id + " deletado com sucesso.");
     }
 
+    @ApiResponseBuscarProjetoPorNome
+    @Operation(summary = "Busca projetos pelo nome completo e partes iniciais do nome",
+            description = "Este endpoint permite buscar projetos por nome completo e partes iniciais do nome")
     @GetMapping("/busca")
     public Page<Projeto> buscarProjetosPorNome(
             @RequestParam String nome,
             @RequestParam Integer pagina,
             @RequestParam Integer itens,
-            @RequestParam(required = false, defaultValue = "false") boolean exactMatch) {
+            @RequestParam(required = false, defaultValue = "false") boolean exactMatch) throws Exception {
 
         return projetoService.buscarProjetosPorNome(nome, pagina, itens, exactMatch);
     }
 
+    @ApiResponseContagemTarefasPorStatus
+    @Operation(summary = "Busca para retornar a contagem de tarefas por status em um projeto.",
+            description = "Este endpoint permite retornar a contagem de tarefas por status em um projeto.")
     @GetMapping("/{id}/contagem-tarefas-status")
-    public TarefasPorStatusDTO contarTarefasPorStatus(@PathVariable String id) throws ProjetoNaoEncontradoException {
+    public TarefasPorStatusDTO contarTarefasPorStatus(@PathVariable String id) throws Exception {
         Map<String, Long> contagem = projetoService.contarTarefasPorStatus(id);
         return new TarefasPorStatusDTO(contagem);
     }
--- src/main/java/hugolini/taskflowapi/controller/TarefaController.java
+++ src/main/java/hugolini/taskflowapi/controller/TarefaController.java
@@ -1,8 +1,12 @@
 package hugolini.taskflowapi.controller;
 
+import hugolini.taskflowapi.annotations.tarefa.ApiResponseCriaTarefa;
+import hugolini.taskflowapi.annotations.tarefa.ApiResponseDeletarTarefa;
 import hugolini.taskflowapi.dto.TarefaDTO;
 import hugolini.taskflowapi.model.Tarefa;
 import hugolini.taskflowapi.service.TarefaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,21 +15,31 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tarefas")
+@Tag(name = "Tarefas", description = "Operações para criar e gerenciar as tarefas")
 public class TarefaController {
 
     @Autowired
     private TarefaService tarefaService;
 
+    @ApiResponseCriaTarefa
+    @Operation(summary = "Cria tarefa e associa a um projeto",
+            description = "Este endpoint permite a criação de tarefas com ID(gerado automaticamente), Titulo(obrigatório), Descrição(opcional), Status(PENDENTE por padrão) e Data de criação(gerado automaticamente)" )
     @PostMapping
     public ResponseEntity <Tarefa> criarTarefa(@RequestBody @Validated TarefaDTO tarefa) throws Exception {
         return ResponseEntity.status(HttpStatus.CREATED).body(tarefaService.criarTarefa(tarefa));
     }
 
+    @ApiResponseDeletarTarefa
+    @Operation(summary = "Atualiza status de uma tarefa",
+            description = "Este endpoint permite a atualização de status: PENDENTE, EM_ANDAMENTO ou CONCLUIDA" )
     @PutMapping("/{id}")
     public ResponseEntity<Tarefa> atualizarStatusTarefa(@PathVariable String id, @RequestBody Tarefa tarefaAtualizada) throws Exception {
         return ResponseEntity.status(HttpStatus.CREATED).body(tarefaService.atualizarStatusTarefa(id, tarefaAtualizada));
     }
 
+    @ApiResponseDeletarTarefa
+    @Operation(summary = "Deleta tarefa associada a um projeto",
+            description = "Este endpoint permite a deleção de tarefas por ID" )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarTarefa(@PathVariable String id) throws Exception {
         return tarefaService.deletarTarefa(id)
