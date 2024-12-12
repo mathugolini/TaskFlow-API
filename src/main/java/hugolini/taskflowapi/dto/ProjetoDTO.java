cbd98faf4077a51e3c0b94ec8a5019d26eef9337
matheus.carneiro <matheus.carneiro@darede.com.br>
2024-12-11 21:37:07 -0300

    fix: removing duplicated annotation

--- src/main/java/hugolini/taskflowapi/dto/ProjetoDTO.java
+++ src/main/java/hugolini/taskflowapi/dto/ProjetoDTO.java
@@ -1,11 +1,14 @@
 package hugolini.taskflowapi.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public class ProjetoDTO {
 
     @NotBlank(message = "O nome do projeto é obrigatório.")
+    @Schema(description = "Nome do projeto", example = "Projeto XYZ", required = true)
     private String nome;
+    @Schema(description = "Descrição do projeto", example = "Este é um projeto de exemplo", required = false)
     private String descricao;
 
     public ProjetoDTO () {}
